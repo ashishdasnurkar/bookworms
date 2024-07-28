b41840f2ccf6a16cfa22994c3dccccdb2f7fbaf3
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-07-28 19:44:32 +1000

    test wip

--- bookworms_internal_test.go
+++ bookworms_internal_test.go
@@ -1,12 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-type testCase struct {
-	bookwormsFile string
-	want          []Bookworm
-	wantErr       bool
-}
 var (
 	handmaidsTale = Book{Author: "Margaret Atwood", Title: "The Handmaid's Tale"}
 	oryxAndCrake  = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
@@ -15,25 +12,76 @@ var (
 )
 
 func TestLoadBookworms_Success(t *testing.T) {
+	tests := map[string]struct {
+		bookwormsFile string
+		want          []Bookworm
+		wantErr       bool
+	}{
+		"file exists": {
+			bookwormsFile: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
+			},
+			wantErr: false,
+		},
+		"file doesn't exist": {
+			bookwormsFile: "testdata/no_file_here.json",
+			want:          nil,
+			wantErr:       true},
+		"invalid JSON": {
+			bookwormsFile: "testdata/invalid.json",
+			want:          nil,
+			wantErr:       true},
+	}
+	for name, testCase := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := loadBookworms(testCase.bookwormsFile)
+			if err != nil && !testCase.wantErr {
+				t.Fatalf("expected an error %s, got none", err.Error())
+			}
+
+			if err == nil && testCase.wantErr {
+				t.Fatalf("expected no error, got one %s", err.Error())
+			}
+
+			if !equalBookworms(got, testCase.want) {
+				t.Fatalf("different result: got %v, expected %v", got, testCase.want)
+			}
+		})
+	}
 }
 
-tests := map[string]testCase {
-	"file exists": {
-		bookwormsFile: "testdata/bookworms.json",
-		want: []Bookworm{
-			{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
-			{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
-		},
-		wantErr: false,
-	},
-	"file doesn't exist": {
-		bookwormsFile: "testdata/no_file_here.json",
-		want:          nil,
-		wantErr:       true,
-	},
-	"invalid JSON": {
-		bookwormsFile: "testdata/invalid.json",
-		want:          nil,
-		wantErr:       true,
+// equalBookworms is a helper to test the equality of two lists of Bookworms.
+func equalBookworms(bookworms, target []Bookworm) bool {
+	if len(bookworms) != len(target) {
+		return false
 	}
+
+	for i := range bookworms {
+		if bookworms[i].Name != target[i].Name {
+			return false
+		}
+
+		if !equalBooks(bookworms[i].Books, target[i].Books) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// equalBooks is a helper to test the equality of two lists of Books.
+func equalBooks(books, target []Book) bool {
+	if len(books) != len(target) {
+		return false
+	}
+
+	for i := range books {
+		if books[i] != target[i] {
+			return false
+		}
+	}
+
+	return true
 }
